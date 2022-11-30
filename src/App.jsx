447f67ce41637a2edfed23ff6a40a67cfaca261c
Ash Jones <ashjones0515@gmail.com>
2022-11-30 13:24:19 -0800

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Login from './pages/Login/Login'
+import AddGiftee from './pages/AddGiftee/AddGiftee'
+import GifteeDetails from './pages/GifteeDetails/GifteeDetails'
+import EditGifteeDetails from './pages/EditGifteeDetails/EditGifteeDetails'
+import AddGift from './pages/AddGift/AddGift'
+import GiftDetails from './pages/GiftDetails/GiftDetails'
+import EditGiftDetails from './pages/EditGiftDetails/EditGiftDetails'
+import GifteeRequestForm from './pages/GifteeRequestForm/GifteeRequestForm'
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.scss';
@@ -7,16 +18,16 @@ export default function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<></>} /> // home page dashboard
-          <Route path='/about' element={<></>} /> // about page
-          <Route path='/login' element={<></>} /> // login page --- Not required for MVP
-          <Route path='/add' element={<></>} /> // add giftee
-          <Route path='/:giftee_id' element={<></>} /> //giftee details
-          <Route path='/:giftee_id/edit' element={<></>} /> // edit giftee
-          <Route path='/:giftee_id/add' element={<></>}/> // add gift for giftee
-          <Route path='/:giftee_id/:gift_id' element={<></>}/> // gift details for giftee
-          <Route path='/:giftee_id/:gift_id/edit' element={<></>} /> // edit gift detials for giftee
-          <Route path='/:used_id/:giftee_id/add' element={<></>} /> // Giftee Gift Request Form
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/add' element={<AddGiftee />} />
+          <Route path='/:giftee_id' element={<GifteeDetails />} />
+          <Route path='/:giftee_id/edit' element={<EditGifteeDetails />} />
+          <Route path='/:giftee_id/add' element={<AddGift />}/>
+          <Route path='/:giftee_id/:gift_id' element={<GiftDetails />}/>
+          <Route path='/:giftee_id/:gift_id/edit' element={<EditGiftDetails />} />
+          <Route path='/:user_id/:giftee_id/add' element={<GifteeRequestForm />} />
         </Routes>
       </BrowserRouter>
     </div>
