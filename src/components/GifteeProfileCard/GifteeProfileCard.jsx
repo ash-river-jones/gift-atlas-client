c9ca246f958e5ae8b25ed67c9690f20666a5eff4
Ash Jones <ashjones0515@gmail.com>
2022-12-02 17:02:53 -0800

    functionality for dashboard home page done

---
+++ src/components/GifteeProfileCard/GifteeProfileCard.jsx
@@ -0,0 +1,80 @@
+import './GifteeProfileCard.scss'
+
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const API_URL = process.env.REACT_APP_SERVER_URL || '';
+
+
+export default function GifteeProfileCard({ giftee_id, address, birthday, email, gift_form_opt_in, name, next_holiday, next_holiday_date, phone, relationship, user_id }) {
+	const [purchasedGifts, setPurchasedGifts] = useState([])
+    const [hasPurchasedGifts, setHasPurchasedGifts] = useState(false)
+    const [daysToBirthday, setDaysToBirthday] = useState('mock data')
+    const [daysToNextHoliday, setDaysToNextHoliday] = useState('mock data')
+
+    useEffect(() => {
+        axios
+            .get(`${API_URL}/giftees/${giftee_id}/purchased`)
+            .then((response) => {
+                if (response.data[0]) {
+                    setPurchasedGifts(response.data[0].item_name)
+                    setHasPurchasedGifts(true)
+                    // console.log(purchasedGifts,hasPurchasedGifts)
+                } else {
+                    setPurchasedGifts([])
+                    setHasPurchasedGifts(false)
+                    // console.log(purchasedGifts,hasPurchasedGifts)
+                }
+            })
+    }, [giftee_id])
+
+
+
+    return (
+		<>
+			<section className='profile'>
+                <ul>
+                    <li className='profile__section'>   
+                        <h4 className='profile__table-heading'>NAME</h4>
+                        <Link to={`/${giftee_id}`}><div className='profile__info-section'><p className='profile__name'>{name}</p></div></Link>
+                    </li>
+                    <li className='profile__section'>   
+                        <h4 className='profile__table-heading'>BIRTHDAY</h4>
+                        <div className='profile__info-section'><p className='profile__info'>{birthday}</p></div>
+                    </li>
+                    <li className='profile__section'>   
+                        <h4 className='profile__table-heading'>NEXT HOLIDAY</h4>
+                        <div className='profile__info-section'><p className='profile__info'>{next_holiday}</p></div>
+                    </li>
+                    {hasPurchasedGifts ? 
+                    <li className='profile__section'>   
+                        <h4 className='profile__table-heading'>GIFT STATUS</h4>
+                        <div className='profile__info-section'><p className='profile__info'>Ordered/Purchasde</p></div>
+                    </li> : <></>
+                    }
+                </ul>
+                <ul>
+                <li className='profile__section'>   
+                        <h4 className='profile__table-heading'>RELATIONSHIP</h4>
+                        <div className='profile__info-section'><p className='profile__info'>{relationship}</p></div>
+                    </li>
+                    <li className='profile__section'>   
+                        <h4 className='profile__table-heading'>DAYS TO BIRTHDAY</h4>
+                        <div className='profile__info-section'><p className='profile__info'>{daysToBirthday}</p></div>
+                    </li>
+                    <li className='profile__section'>   
+                        <h4 className='profile__table-heading'>DAYS UNTIL</h4>
+                        <div className='profile__info-section'><p className='profile__info'>{daysToNextHoliday}</p></div>
+                    </li>
+                    {hasPurchasedGifts ? 
+                    <li className='profile__section'>   
+                        <h4 className='profile__table-heading'>GIFT PURCHASED</h4>
+                        <div className='profile__info-section'><p className='profile__info'>{purchasedGifts}</p></div>
+                    </li>: <></>
+                    }
+                </ul>
+            </section>
+		</>
+	);
+}
--- src/components/HomeCard/HomeCard.jsx
+++ src/components/HomeCard/HomeCard.jsx
@@ -1,9 +1,71 @@
 import './HomeCard.scss';
+import GifteeProfileCard from '../GifteeProfileCard/GifteeProfileCard';
+
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_SERVER_URL || '';
 
 export default function HomeCard() {
+	const [gifteeData, setGifteeData] = useState([])
+	const [searchInput, setSearchInput] = useState([])
+
+	useEffect(() => {
+		axios
+			.get(`${API_URL}/giftees`)
+			.then((response) => {
+				console.log(response.data)
+				setGifteeData(response.data)
+			})
+			.catch((err) => {console.log(err)})
+	},[])
+	
 	return (
 		<>
-			
+			<main className='dash'>
+				<section className='dash__header'>
+					<h1 className='dash__title'>Giftees</h1>
+					<div className="dash__wrapper">
+						<div className="dash__search-wrapper"><input className='dash__search' type='text' placeholder='Search...' onChange={(e)=>{setSearchInput(e.target.value.toLocaleLowerCase())}}/></div>
+						<div className='dash__new-giftee-wrapper'><p className='dash__new-giftee-btn'>+ Add New Giftee</p></div>
+					</div>
+				</section>
+				<section className='giftees'>
+
+				{gifteeData
+						.filter((data) => {
+							return (
+								data.name.toLocaleLowerCase().includes(searchInput) ||
+								data.relationship.toLocaleLowerCase().includes(searchInput) ||
+								data.birthday.toLocaleLowerCase().includes(searchInput) ||
+								data.next_holiday.toLocaleLowerCase().includes(searchInput) ||
+								data.next_holiday_date.toLocaleLowerCase().includes(searchInput)
+							);
+						})
+						.map((data) => (
+							<div key={data.giftee_id}>
+								<GifteeProfileCard
+									key={data.id}
+									giftee_id={data.giftee_id}
+									address={data.address}
+									birthday={data.birthday}
+									email={data.birthday}
+									gift_form_opt_in={data.gift_form_opt_in}
+									name={data.name}
+									next_holiday={data.next_holiday}
+									next_holiday_date={data.next_holiday_date}
+									phone={data.phone}
+									relationship={data.relationship}
+									user_id={data.user_id}
+								/>
+
+							</div>
+						))
+					}
+
+				</section>
+			</main>
 		</>
 	);
 }
